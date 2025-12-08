9e27bd85aa034c35ce857544cc0a196a7ff6e90e
sgideon1 <sophie_gideon@brown.edu>
2025-12-08 15:52:26 -0500

    Combined regression and recommendation

--- recommendation_system.R
+++ recommendation_system.R
@@ -4,22 +4,41 @@ library(lsa)
 
 source("reviews.R")
 source("demographic_analysis.R")
-source("similarpersonarecs.R")
-source("regression.R")
 source("stability.R")
 
-# include only relevant products and utilize skin tone bucket
+relevant_cats <- c(skin_type_categories, skin_tone_categories)
+
+# filter to only skincare categories where demographic matters
+df_rel <- df %>%
+  filter(secondary_category %in% relevant_cats)
+
+# create skin tone buckets for more simple groupings
 df_rel <- df %>%
   filter(secondary_category %in% c(
     "Moisturizers","Treatments","Cleansers",
     "Masks","Sunscreen","Self Tanners")) %>%
   mutate(skin_tone_bucket = case_when(
-      skin_tone %in% c("fair", "porcelain", "fairLight") ~ "Fair",
-      skin_tone %in% c("light", "lightMedium") ~ "Light",
-      skin_tone %in% c("medium", "mediumTan", "olive") ~ "Medium",
-      skin_tone %in% c("tan") ~ "Tan",
-      skin_tone %in% c("deep", "rich", "dark") ~ "Deep",
-      TRUE ~ NA_character_))
+    skin_tone %in% c("fair", "porcelain", "fairLight") ~ "Fair",
+    skin_tone %in% c("light", "lightMedium") ~ "Light",
+    skin_tone %in% c("medium", "mediumTan", "olive") ~ "Medium",
+    skin_tone %in% c("tan") ~ "Tan",
+    skin_tone %in% c("deep", "rich", "dark") ~ "Deep",
+    TRUE ~ NA_character_
+    ),
+    high_rating = rating >=4
+  )
+
+# predict probability of a user granting a high rating (4+)
+glm_interact <- glm(
+  high_rating ~ 
+    skin_type * secondary_category +
+    skin_tone_bucket * secondary_category +
+    price_usd,
+  data = df_rel,
+  family = binomial
+)
+summary(glm_interact)
+
 
 # user demographic dummy matrix
 user_demo <- df_rel %>%
@@ -142,47 +161,3 @@ recommend_similar_products <- function(df_rel,
 }
 
 
-recommend_similar_products(df_rel, user_vectors,
-                           persona_skin_type = "combination",
-                           persona_skin_tone = "Deep",
-                           price_min = 0,
-                           price_max = 20,
-                           category = "Moisturizers",
-                           n_recs = 5
-)
-
-recommend_similar_products(df_rel, user_vectors,
-                           persona_skin_type = "oily",
-                           persona_skin_tone = "Fair",
-                           price_min = 0,
-                           price_max = 50,
-                           category = "Sunscreen",
-                           n_recs = 5
-)
-
-recommend_similar_products(df_rel, user_vectors,
-                           persona_skin_type = "dry",
-                           persona_skin_tone = "Medium",
-                           price_min = 0,
-                           price_max = 15,
-                           category = "Cleansers",
-                           n_recs = 5
-)
-
-recommend_similar_products(df_rel, user_vectors,
-                           persona_skin_type = "normal",
-                           persona_skin_tone = "Light",
-                           price_min = 0,
-                           price_max = 30,
-                           category = "Cleansers",
-                           n_recs = 5
-)
-
-recommend_similar_products(df_rel, user_vectors,
-                           persona_skin_type = "combination",
-                           persona_skin_tone = "Tan",
-                           price_min = 0,
-                           price_max = 100,
-                           category = "Cleansers",
-                           n_recs = 5
-)
